edf49256009070df39ad3584d554aecfaea6bd7c
Simara08 <7knywpc@code.edu.az>
2022-03-22 23:11:52 +0400

    created_kokaltiMethod

---
+++ kokaltimethod/kokaltimethod/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace kokaltimethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 42;
+            num = Scuare( num);
+
+            Console.WriteLine(num);
+        }
+        static int Scuare(  int number)
+        {
+            int i=0 ;
+
+            while (i*i<=number)
+            {
+                i++;
+               
+            }
+                
+            
+            if (i*i==0)
+            {
+                Console.WriteLine(i);
+            }
+
+            return i;
+            
+           
+        }
+    }
+}
