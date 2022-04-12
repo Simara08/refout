c989a3bfa157957e37fe14eae8315ec500b9b031
Simara08 <100687456+Simara08@users.noreply.github.com>
2022-04-12 18:54:06 +0400

    Add files via upload

---
+++ CourseManagment/CourseManagment/NewGrup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseManagment {
+    class NewGrup
+    {
+        private string _no;
+        public string No { get; set; }
+        public string Category { get; set; }
+        
+        private bool _isonline;
+        public bool IsOnline { get; set; }
+        private int _limit;
+        public int Limit
+        {
+            get
+            {
+                if (_isonline == true)
+                {
+                    return 15;
+                }
+                else
+                {
+                    return 10;
+                }
+            }
+            set
+            {
+                value = _limit;
+            }
+        }
+        public NewGrup(string no,string category,bool isonline)
+        {
+            No = no;
+            Category = category;
+            IsOnline = isonline;
+        }
+        
+        
+       
+
+    }
+}
+
+//      private bool _isonline;
+//public bool IsOnline
+
+    //get
+    //{
+    //    if (_limit == 15 || _limit == 10)
+    //    {
+    //        return true;
+    //    }
+    //    else
+    //    {
+    //        return false;
+    //    }
+    //}
+    //set
+    //{
+    //    value = _isonline;
+    //}
+
+
+    
+
---
+++ CourseManagment/CourseManagment/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseManagment
+{
+    class Student
+    {
+        //Fullname Group No Type
+        public string FullNmae { get; set; }
+        public int GrupNo { get; set; }
+        public bool Type { get; set; }
+        public Student(string fullname,int grupno,bool type)
+        {
+            FullNmae = fullname;
+            GrupNo = grupno;
+            Type = type;
+        }
+        public override string ToString()
+        {
+            var result = Type ? "zemanetli" : "Zemanetsiz";
+            return $"Fullname:{FullNmae} Grupno{GrupNo} Type{Type}";
+        }
+    }
+}
