d15dd5e2df89b27a69696cde5dfaffcaf04a7e5e
Simara08 <100687456+Simara08@users.noreply.github.com>
2022-04-14 16:18:45 +0400

    Add files via upload

---
+++ Serilization/Serilization/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serilization
+{
+    class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public int Price { get; set; }
+
+
+    }
+}
